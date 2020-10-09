2dc9a6ef046099fbdd9ffe8615492a43c579b82a
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-09 13:45:55 -0400

    :sparkles: create database and first table

--- lib/model/database.dart
+++ lib/model/database.dart
@@ -1,32 +1,83 @@
-// import 'package:sqflite/sqflite.dart';
-// import 'dart:io';
-// import 'package:path_provider/path_provider.dart';
+import 'dart:async';
 
-// class Database {
-//   Database _database;
+import 'package:raid_organizer/model/game.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
-//   Future<Database> get database async {
-//     if (_database != null) {
-//       return _database;
-//     } else {
-//       // Créer la database
-//     }
-//   }
+class DatabaseClient {
+  Database _database;
 
-//   Future create() async {
-//     Directory directory = await getApplicationDocumentsDirectory();
-//     String databaseDirectory = join(directory.path, 'database.db');
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    } else {
+      // Créer la database
+      _database = await create();
+      return _database;
+    }
+  }
 
-//     var bdd =
-//         await openDatabase(databaseDirectory, version: 1, onCreate: _onCreate);
-//     return bdd;
-//   }
+  Future create() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String databaseDirectory = join(directory.path, 'database.db');
 
-//   Future _onCreate(Database db, int version) async {
-//     await db.execute('''
-//     CREATE TABLE Game(id INTEGER PRIMARY KEY, name TEXT NOT NULL, image TEXT NOT NULL, description TEXT NOT NULL)
-//     ''');
-//   }
+    var bdd =
+        await openDatabase(databaseDirectory, version: 1, onCreate: _onCreate);
+    return bdd;
+  }
 
-//   // Ajout des données
-// }
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE Game(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+    )''');
+  }
+
+  // Ajout des données
+  Future<Game> addGame(Game game) async {
+    Database myDatabase = await database;
+    game.id = await myDatabase.insert('game', game.toMap());
+    return game;
+  }
+
+  // Modification des données
+  Future<int> updateGame(Game game) async {
+    Database myDatabase = await database;
+    return myDatabase
+        .update('game', game.toMap(), where: 'id = ?', whereArgs: [game.id]);
+  }
+
+  Future<Game> upsertGame(Game game) async {
+    Database myDatabase = await database;
+    if (game.id == null) {
+      game.id = await myDatabase.insert('game', game.toMap());
+    } else {
+      await myDatabase
+          .update('game', game.toMap(), where: 'id = ?', whereArgs: [game.id]);
+    }
+    return game;
+  }
+
+  // Suppression des données
+  Future<int> deleteGame(int id, String table) async {
+    Database myDatabase = await database;
+    return await myDatabase.delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+
+  // Lecture des données
+  Future<List<Game>> showGames() async {
+    Database myDatabase = await database;
+    List<Map<String, dynamic>> result =
+        await myDatabase.rawQuery("SELECT * FROM game");
+    List<Game> games = [];
+    result.forEach((map) {
+      Game game = new Game();
+      game.fromMap(map);
+      games.add(game);
+    });
+    return games;
+  }
+}
