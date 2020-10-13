eb73e0324c33ed4c57df9107dedf8d044b6b4e17
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-13 09:08:42 -0400

    :zap: test improve

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+          primarySwatch: Colors.grey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          pageTransitionsTheme: PageTransitionsTheme(builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder()
+          })),
       home: HomeController(title: 'Raid Organizer'),
     );
   }
