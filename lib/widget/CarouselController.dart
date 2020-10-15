59d7019a8bb5d25875be503b08182784ffbbf891
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-15 17:56:32 -0400

    :zap: add route to game detail on carousel image and add border radius on them

--- lib/widget/CarouselController.dart
+++ lib/widget/CarouselController.dart
@@ -4,6 +4,8 @@ import 'package:raid_organizer/model/game.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'dart:developer';
 
+import 'package:raid_organizer/widget/GameDetails.dart';
+
 class Carousel extends StatefulWidget {
   @override
   _CarouselState createState() => _CarouselState();
@@ -34,15 +36,21 @@ class _CarouselState extends State<Carousel> {
           enlargeCenterPage: true,
           scrollDirection: Axis.horizontal,
         ),
-        items: games.map((i) {
+        items: games.map((game) {
           return Builder(
             builder: (BuildContext context) {
-              return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Image.network(
-                    'https://picsum.photos/id/${i.id + 10}/200/120/',
-                  ));
+              return GestureDetector(
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(
+                        'https://picsum.photos/id/${game.id + 10}/200/120/',
+                      )),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext buildContext) {
+                      return GameDetail(game);
+                    }));
+                  });
             },
           );
         }).toList(),
@@ -51,7 +59,6 @@ class _CarouselState extends State<Carousel> {
   }
 
   void getGames() {
-    inspect(games);
     DatabaseClient().showGames().then((games) {
       setState(() {
         this.games = games;
