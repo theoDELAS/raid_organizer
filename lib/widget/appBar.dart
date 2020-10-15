72b58e6dc1888bd32d346b4a641a978fcaf9dff8
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-15 17:55:00 -0400

    :sparkles: appbar externalization

---
+++ lib/widget/appBar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Widget appbar(BuildContext buildContext) {
+  String title = 'RAID ORGANIZER';
+  return AppBar(
+    backgroundColor: Colors.blueGrey.shade700,
+    leading: CircleAvatar(
+      radius: 50,
+      backgroundImage: AssetImage('images/test4.jpg'),
+    ),
+    title: Center(
+      child: Text(
+        title,
+        style: TextStyle(
+            fontSize: 25, fontWeight: FontWeight.bold, fontFamily: 'Jost'),
+      ),
+    ),
+    actions: <Widget>[
+      IconButton(
+        iconSize: 40,
+        icon: Icon(
+          Icons.group,
+          color: Color.fromRGBO(2, 196, 131, 1),
+        ),
+        onPressed: () {
+          // do something
+        },
+      ),
+    ],
+  );
+}
