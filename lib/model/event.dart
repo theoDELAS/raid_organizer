400ba7f85a611ea223d495b3adcacfcfa20de4b1
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-16 18:01:40 -0400

    :sparkles: create model event

---
+++ lib/model/event.dart
@@ -0,0 +1,34 @@
+class Evenement {
+  int id;
+  String title;
+  String description;
+  DateTime date;
+  int slots;
+  bool is_private;
+  int user;
+
+  fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.title = map['title'];
+    this.date = map['date'];
+    this.description = map['description'];
+    this.slots = map['slots'];
+    this.is_private = map['is_private'];
+    this.user = map['user'];
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'title': this.title,
+      'date': this.date,
+      'description': this.description,
+      'slots': this.slots,
+      'is_private': this.is_private,
+      'user': this.user
+    };
+    if (id != null) {
+      map['id'] = this.id;
+    }
+    return map;
+  }
+}
