e5453751d01aee3820e4d4e5f5bddf1864e353e7
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-16 11:44:32 -0400

    :zap: add some field to class

--- lib/model/game.dart
+++ lib/model/game.dart
@@ -1,21 +1,21 @@
 class Game {
   int id;
   String name;
-  // String image;
-  // String description;
+  String image;
+  String description;
 
   fromMap(Map<String, dynamic> map) {
     this.id = map['id'];
     this.name = map['name'];
-    // this.image = map['image'];
-    // this.description = map['description'];
+    this.image = map['image'];
+    this.description = map['description'];
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
       'name': this.name,
-      // 'image': this.image,
-      // 'description': this.description
+      'image': this.image,
+      'description': this.description
     };
     if (id != null) {
       map['id'] = this.id;
