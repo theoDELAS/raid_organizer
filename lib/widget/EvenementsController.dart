d346781c31a8f0feffb81ea2330b6b18021f3a85
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-22 10:41:40 -0400

    :sparkles: add a datetime picker

--- lib/widget/EvenementsController.dart
+++ lib/widget/EvenementsController.dart
@@ -9,7 +9,8 @@ import 'package:raid_organizer/model/database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:raid_organizer/widget/GameDetailsController.dart';
 import 'dart:async';
-import 'package:intl/intl.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:provider/provider.dart';
 
 import 'appBar.dart';
 
@@ -26,6 +27,8 @@ class EvenementsController extends StatefulWidget {
 }
 
 class _EvenementsControllerState extends State<EvenementsController> {
+  get game => this.game;
+
   @override
   void initState() {
     super.initState();
@@ -54,9 +57,9 @@ class _EvenementsControllerState extends State<EvenementsController> {
           children: <Widget>[
             SizedBox(height: 30),
             FlatButton(
-              onPressed: (() => add(null)),
+              onPressed: (() => create(null)),
               child: Text(
-                "Créer un évènement",
+                "Créer un évènement".toUpperCase(),
                 style: TextStyle(fontSize: 20, fontFamily: 'Jost'),
               ),
               color: Color.fromRGBO(2, 196, 131, 1),
@@ -110,7 +113,7 @@ class _EvenementsControllerState extends State<EvenementsController> {
         )));
   }
 
-  Future<Null> add(Evenement evenement) async {
+  Future<Null> create(Evenement evenement) async {
     await showDialog(
         context: context,
         builder: (BuildContext buildContext) {
@@ -162,47 +165,44 @@ class _EvenementsControllerState extends State<EvenementsController> {
                               ],
                             ),
                             SizedBox(height: 20),
-                            Text(
-                              "Date et heure de l'évènement".toUpperCase(),
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontFamily: 'Jost',
-                                // color: Color.fromRGBO(2, 196, 131, 1)
-                                color: Colors.white,
+                            // Text(
+                            //   "Date et heure de l'évènement".toUpperCase(),
+                            //   style: TextStyle(
+                            //     fontSize: 20,
+                            //     fontFamily: 'Jost',
+                            //     color: Colors.white,
+                            //   ),
+                            // ),
+                            // SizedBox(height: 10),
+                            // Row(
+                            //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            //   children: [
+                            FlatButton(
+                              color: Colors.black54,
+                              child: Text(
+                                (date == null)
+                                    ? "Date et heure de l'évènement"
+                                        .toUpperCase()
+                                    : date,
+                                style: TextStyle(
+                                    color: Colors.white, fontFamily: 'Jost'),
                               ),
+                              onPressed: () {
+                                DatePicker.showDateTimePicker(context,
+                                    showTitleActions: true,
+                                    minTime: DateTime.now(),
+                                    maxTime: DateTime(2020, 12, 31),
+                                    onChanged: (date) {
+                                  print('change $date');
+                                }, onConfirm: (date) {
+                                  print('confirm $date');
+                                },
+                                    currentTime: DateTime.now(),
+                                    locale: LocaleType.fr);
+                              },
                             ),
-                            SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                FlatButton(
-                                  color: Colors.black54,
-                                  child: Text(
-                                    (date == null)
-                                        ? "Sélectionnez une date"
-                                        : DateFormat("dd-MM-yyyy")
-                                            .format(date)
-                                            .toString(),
-                                    style: TextStyle(
-                                        fontFamily: 'Jost',
-                                        color: Colors.white),
-                                  ),
-                                  onPressed: showDate,
-                                ),
-                                FlatButton(
-                                  color: Colors.black54,
-                                  child: Text(
-                                    (hour == null)
-                                        ? "Sélectionnez une heure"
-                                        : hour.toString(),
-                                    style: TextStyle(
-                                        fontFamily: 'Jost',
-                                        color: Colors.white),
-                                  ),
-                                  onPressed: showHour,
-                                )
-                              ],
-                            ),
+                            //   ],
+                            // ),
                             SizedBox(height: 20),
                             myTextField(TypeTextField.description,
                                 "Description de l'évènement"),
@@ -214,74 +214,80 @@ class _EvenementsControllerState extends State<EvenementsController> {
                             ),
                             SizedBox(height: 5),
                             Container(
-                                width: 115,
-                                child: TextField(
-                                    decoration: InputDecoration(
-                                        labelText: "(Entre 2 et 5)",
-                                        labelStyle: TextStyle(
-                                          fontFamily: 'Jost',
-                                          color: Color.fromRGBO(2, 196, 131, 1),
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Color.fromRGBO(
-                                                    2, 196, 131, 1))),
-                                        border: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Colors.transparent))),
-                                    keyboardType: TextInputType.number,
-                                    inputFormatters: <TextInputFormatter>[
-                                      FilteringTextInputFormatter.digitsOnly
-                                    ]))
+                              width: 120,
+                              child: TextFormField(
+                                  validator: (val) {
+                                    final supportValue = int.tryParse(val);
+                                    return supportValue >= 2 &&
+                                            supportValue <= 5
+                                        ? "Le nombre de participants doit être entre 2 et 5"
+                                        : null;
+                                  },
+                                  decoration: InputDecoration(
+                                      labelText: "(Entre 2 et 5)",
+                                      labelStyle: TextStyle(
+                                        fontFamily: 'Jost',
+                                        color: Color.fromRGBO(2, 196, 131, 1),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Color.fromRGBO(
+                                                  2, 196, 131, 1))),
+                                      border: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Colors.transparent))),
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.digitsOnly
+                                  ]),
+                            ),
+                            SizedBox(height: 30),
+                            FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                color: Color.fromRGBO(1, 196, 131, 1),
+                                padding: EdgeInsets.all(7),
+                                onPressed: () {
+                                  Provider.of(context, listen: false);
+                                  // Ajouter à la base de données
+                                  if (title != null) {
+                                    Map<String, Object> map = {'title': title};
+                                    if (image != null) {
+                                      map['image'] = image;
+                                    }
+                                    if (description != null) {
+                                      map['description'] = description;
+                                    }
+                                    if (game != null) {
+                                      map['game_id'] = game.id;
+                                    }
+                                    Evenement evenement = new Evenement();
+                                    print(evenement);
+                                    evenement.fromMap(map);
+                                    print(evenement);
+                                    DatabaseClient()
+                                        .upsertEvenement(evenement)
+                                        .then((value) {
+                                      game.id = null;
+                                      title = null;
+                                      image = null;
+                                      description = null;
+                                    });
+                                    print(evenement);
+                                  }
+                                },
+                                child: Text(
+                                  "Créer l'évènement !".toUpperCase(),
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Jost',
+                                      fontSize: 20),
+                                )),
                           ],
                         ))
                   ],
                 )),
-              )
-
-              // title: Text('Ajouter un jeu'),
-              // content: Card(
-              //     child: Column(children: <Widget>[
-              //   myTextField(TypeTextField.title, "Nom du jeu"),
-              //   myTextField(TypeTextField.description, "Image du jeu"),
-              //   myTextField(TypeTextField.slots, "Description du jeu"),
-              //   myTextField(TypeTextField.is_private, "Description du jeu"),
-              //   myTextField(TypeTextField.date, "Description du jeu"),
-              // ])),
-              // actions: <Widget>[
-              //   FlatButton(
-              //       onPressed: (() => Navigator.pop(buildContext)),
-              //       child: Text('Annuler')),
-              //   FlatButton(
-              //     onPressed: () {
-              //       // Ajouter à la base de données
-              //       if (title != null) {
-              //         Map<String, dynamic> map = {'name': title};
-              //         if (image != null) {
-              //           map['image'] = image;
-              //         }
-              //         if (description != null) {
-              //           map['description'] = description;
-              //         }
-              //         Evenement evenement = new Evenement();
-              //         evenement.fromMap(map);
-              //         DatabaseClient().upsertEvenement(evenement).then((value) {
-              //           title = null;
-              //           image = null;
-              //           description = null;
-              //           Navigator.pop(context);
-              //         });
-              //       }
-              //     },
-              //     child: Text(
-              //       'Valider',
-              //       style: TextStyle(
-              //         color: Colors.green,
-              //       ),
-              //     ),
-              //   )
-              // ]
-              );
+              ));
         });
   }
 
@@ -307,6 +313,8 @@ class _EvenementsControllerState extends State<EvenementsController> {
       });
     }
   }
+
+  Future<Null> add(Evenement evenement) {}
 
   TextField myTextField(TypeTextField type, String label) {
     return TextField(
@@ -344,14 +352,6 @@ class _EvenementsControllerState extends State<EvenementsController> {
       },
     );
   }
-
-  // void getGames() {
-  //   DatabaseClient().showGames().then((games) {
-  //     setState(() {
-  //       this.games = games;
-  //     });
-  //   });
-  // }
 }
 
 enum TypeTextField { title, date, hour, description, slots, is_private }
