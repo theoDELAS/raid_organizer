5fd4f97fa95efdf47f5e2a84b6090f3b224d2aef
maximelarrieu <maxime.larrieu@ynov.com>
2020-10-30 10:57:39 -0400

    :art: change input fields color

--- lib/widget/ConnexionController.dart
+++ lib/widget/ConnexionController.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:raid_organizer/model/user.dart';
-import 'package:raid_organizer/model/game.dart';
 import 'package:raid_organizer/widget/HomeController.dart';
 import 'package:raid_organizer/model/database.dart';
 
@@ -50,16 +49,36 @@ class _ConnexionControllerState extends State<ConnexionController> {
               children: <Widget>[
                 TextFormField(
                   decoration: InputDecoration(
-                      labelText: "Nom d'utilisateur",
-                      border: OutlineInputBorder()),
-                  validator: (val) =>
-                      val.isEmpty ? 'Veuillez saisir votre e-mail' : null,
+                    labelText: "Nom d'utilisateur",
+                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle(
+                        fontFamily: 'Jost',
+                        color: Color.fromRGBO(2, 196, 131, 1)),
+                    fillColor: Colors.blueGrey.shade700,
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color.fromRGBO(2, 196, 131, 1))),
+                  ),
+                  validator: (val) => val.isEmpty
+                      ? "Veuillez saisir votre nom d'utilisateur"
+                      : null,
                   onChanged: (val) => username = val,
                 ),
                 SizedBox(height: 30.0),
                 TextFormField(
                   decoration: InputDecoration(
-                      labelText: 'Mot de passe', border: OutlineInputBorder()),
+                    labelText: 'Mot de passe',
+                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle(
+                        fontFamily: 'Jost',
+                        color: Color.fromRGBO(2, 196, 131, 1)),
+                    fillColor: Colors.blueGrey.shade700,
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color.fromRGBO(2, 196, 131, 1))),
+                  ),
                   obscureText: true,
                   onChanged: (val) => password = val,
                   validator: (val) => val.length < 6
