899e81eef8efadb4724494054ced44fe90c1a8a7
maximelarrieu <maxime.larrieu@ynov.com>
2020-11-02 06:36:51 -0500

    :zap: pass game to form

--- lib/widget/EvenementsController.dart
+++ lib/widget/EvenementsController.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -54,13 +55,13 @@ class _EvenementsControllerState extends State<EvenementsController> {
   String title;
   String image;
   String description;
-  num slots;
+  String slots;
+  String game_id;
   DateTime date;
   // bool is_private = false;
 
   @override
   Widget build(BuildContext context) {
-    // print(evenements.map((e) => e.slots));
     return Scaffold(
         resizeToAvoidBottomPadding: true,
         backgroundColor: Colors.blueGrey.shade700,
@@ -73,7 +74,7 @@ class _EvenementsControllerState extends State<EvenementsController> {
             FlatButton(
               onPressed: (() => Navigator.push(context,
                       MaterialPageRoute(builder: (BuildContext buildContext) {
-                    return new EvenementsFormController();
+                    return new EvenementsFormController(widget.game);
                   }))),
               child: Text(
                 "Créer un évènement".toUpperCase(),
@@ -101,139 +102,141 @@ class _EvenementsControllerState extends State<EvenementsController> {
                 style: TextStyle(fontSize: 20, fontFamily: 'Jost')),
             SizedBox(height: 10),
             for (var event in evenements)
-              Slidable(
-                actionPane: SlidableDrawerActionPane(),
-                actionExtentRatio: 0.25,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  height: MediaQuery.of(context).size.height * 0.13,
-                  margin: const EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3))
-                    ],
-                    image: DecorationImage(
-                      image: NetworkImage(widget.game.image),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.4), BlendMode.dstATop),
+              if (widget.game.id.toString() == event.game_id)
+                Slidable(
+                  actionPane: SlidableDrawerActionPane(),
+                  actionExtentRatio: 0.25,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    margin: const EdgeInsets.only(bottom: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3))
+                      ],
+                      image: DecorationImage(
+                        image: NetworkImage(widget.game.image),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.4), BlendMode.dstATop),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          color: Colors.white,
+                          width: 5,
+                          height: 110,
+                        ),
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.only(right: 15),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(
+                                  color: Colors.white,
+                                  width: 1,
+                                  style: BorderStyle.solid),
+                            ),
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.keyboard_arrow_right,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                  margin: const EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    "Username",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Jost',
+                                      fontSize: 16,
+                                    ),
+                                  )),
+                              SizedBox(height: 40),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.person,
+                                    color: Colors.white,
+                                    size: 24.0,
+                                  ),
+                                  Text(
+                                    "0/" + event.slots,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Jost',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            children: [
+                              Container(
+                                  margin:
+                                      const EdgeInsets.only(left: 150, top: 75),
+                                  child: Text(
+                                    "Début : 20h15",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Jost',
+                                      fontSize: 15,
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        color: Colors.white,
-                        width: 5,
-                        height: 110,
-                      ),
-                      Container(
-                        height: 50,
-                        margin: EdgeInsets.only(right: 15),
+                  actions: <Widget>[
+                    Container(
+                        height: MediaQuery.of(context).size.height * 0.13,
+                        margin: const EdgeInsets.only(bottom: 20),
                         decoration: BoxDecoration(
-                          border: Border(
-                            right: BorderSide(
-                                color: Colors.white,
-                                width: 1,
-                                style: BorderStyle.solid),
-                          ),
-                        ),
-                        child: Container(
-                          padding: EdgeInsets.only(right: 5),
-                          child: Icon(
-                            Icons.keyboard_arrow_right,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                                margin: const EdgeInsets.only(top: 10),
-                                child: Text(
-                                  "Username",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Jost',
-                                    fontSize: 16,
-                                  ),
-                                )),
-                            SizedBox(height: 40),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.person,
-                                  color: Colors.white,
-                                  size: 24.0,
-                                ),
-                                Text(
-                                  "0/" + event.slots,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Jost',
-                                  ),
-                                ),
-                              ],
-                            ),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3))
                           ],
                         ),
-                      ),
-                      Expanded(
-                        flex: 8,
-                        child: Column(
-                          children: [
-                            Container(
-                                margin:
-                                    const EdgeInsets.only(left: 150, top: 75),
-                                child: Text(
-                                  "Début : 20h15",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Jost',
-                                    fontSize: 15,
-                                  ),
-                                ))
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                        child: IconSlideAction(
+                            caption: 'Rejoindre',
+                            color: Color.fromRGBO(1, 191, 136, 1),
+                            icon: Icons.control_point_duplicate_rounded,
+                            foregroundColor: Colors.white,
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (BuildContext buildContext) {
+                                return HomeController();
+                              }));
+                            }))
+                  ],
                 ),
-                actions: <Widget>[
-                  Container(
-                      height: MediaQuery.of(context).size.height * 0.13,
-                      margin: const EdgeInsets.only(bottom: 20),
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3))
-                        ],
-                      ),
-                      child: IconSlideAction(
-                          caption: 'Rejoindre',
-                          color: Color.fromRGBO(1, 191, 136, 1),
-                          icon: Icons.control_point_duplicate_rounded,
-                          foregroundColor: Colors.white,
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(
-                                builder: (BuildContext buildContext) {
-                              return HomeController();
-                            }));
-                          }))
-                ],
-              ),
             SizedBox(height: 40),
             Text(
               "Activités en cours".toUpperCase(),
